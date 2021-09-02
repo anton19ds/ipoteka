e9b1ba488874f0320d99a2a7c240915e77a64164
anton19ds <bar2018ka@gmail.com>
2021-09-03 00:25:11 +0300

    aff14

--- config/db.php
+++ config/db.php
@@ -2,11 +2,12 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=ipoteka',
-    'username' => 'ttuser',
-    'password' => '12345',
+    'dsn' => 'mysql:host=localhost;dbname=c34807_dev_ipoteka_na4u_ru',
+    'username' => 'c34807_dev_ipoteka_na4u_ru',
+    'password' => 'GaShuVeptewed20',
     'charset' => 'utf8',
 
+    
     // Schema cache options (for production environment)
     //'enableSchemaCache' => true,
     //'schemaCacheDuration' => 60,
