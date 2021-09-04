4849f68b1e7844ae8d66a3b2ae8ca15b353cbf6b
anton19ds <bar2018ka@gmail.com>
2021-09-04 14:52:38 +0300

    34sdf

--- widgets/views/admin_menu.php
+++ widgets/views/admin_menu.php
@@ -14,10 +14,10 @@
                             <li><a href="/admin/articles">Статьи</a></li>
                             <li><a href="/admin/document">Блок документов</a></li>
                             <li><a href="/admin/career">Карьера / Вакансии</a></li>
-                            <li><a href="">Кейсы</a></li>
+                            <!-- <li><a href="">Кейсы</a></li> -->
                             <li><a href="/admin/page/about-us">О нас</a></li>
                             <li><a href="/admin/page/reviews">Отзывы</a></li>
-                            <li><a href="">Партнеры</a></li>
+                            <!-- <li><a href="">Партнеры</a></li> -->
                             <li><a href="/admin/question">Вопрос и ответ</a></li>
                             <li><a href="/admin/setting">Настройка сайта</a></li>
                         </ul>
